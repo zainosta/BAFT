a2119777ff69ed302c58e4cfeb2dc66084f1e0a0
Zainosta <76891223+zainosta@users.noreply.github.com>
2026-01-08 22:04:05 +0300

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http, {
     cors: { origin: "*" }
 });
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 4000;
 const JWT_SECRET = process.env.JWT_SECRET || 'your-secret-key';
 
 // Real-time Socket logic
@@ -1258,5 +1258,6 @@ checkSchema().then(() => {
         console.log(`\n⚠️  Make sure your phone is on the same WiFi network!\n`);
     });
 });
+
 
 
